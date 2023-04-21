b9fb40dd0395e1bb16ff56478abc10c1bcc94b28
Jessica McPhaul <120339830+texaschikkita@users.noreply.github.com>
2023-04-20 20:21:04 -0500

    Add files via upload

---
+++ JTM/images/air_incidents.sql
@@ -0,0 +1,23 @@
+
+
+CREATE TABLE "air_incidents" (
+    "month" varchar  NOT NULL ,
+    "day" int  NOT NULL ,
+    "year" int  NOT NULL ,
+    "time" time  NOT NULL ,
+    "airline" varchar  NOT NULL ,
+    "flight_no" int  NOT NULL ,
+    "coordinates" varchar  NOT NULL ,
+    "location" varchar  NOT NULL ,
+    "fault" varchar  NOT NULL ,
+    "origination" varchar  NOT NULL ,
+    "destination" varchar  NOT NULL ,
+    "route" varchar  NOT NULL ,
+    "aircraft" varchar  NOT NULL ,
+    "total_aboard" int  NOT NULL ,
+    "passengers_aboard" int  NOT NULL ,
+    "total_fatalities" int  NOT NULL ,
+    "passenger_fatalities" int  NOT NULL ,
+    "ground_registration" varchar  NOT NULL )
+
+
